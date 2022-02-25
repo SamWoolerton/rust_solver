09a49a4df687b2f9cb760f209be8795545e56650
Sam Woolerton <samuelwoolerton@gmail.com>
2022-02-25 20:43:39 +1300

    Words list

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 mod logic;
 mod utility;
+mod words;
 
 fn main() {
     logic::example();
 
     let t = utility::timed(|| {
-        let mut sum: u64 = 0;
-        for x in 1..100000 {
-            sum += x
+        let mut count_letters: usize = 0;
+        for w in words::VALID_WORDS {
+            count_letters += w.len()
         }
-        println!("{:?}", sum);
+        println!("{:?}", count_letters);
+
+        let mut count_words: usize = 0;
+        for _guess in words::VALID_WORDS {
+            for _answer in words::VALID_WORDS {
+                count_words += 1
+            }
+        }
+        println!("{:?}", count_words);
     });
 
     println!("{:?}", t);
